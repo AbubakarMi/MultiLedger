6da8e25141b973ea8b4ac9fb8b03b94d6472698c
Abubakar Mi <abubakarmi131@gmail.com>
2025-08-26 12:23:23 +0100

    Transaction

--- MultiLedger.Domain/Entities/Transaction.cs
+++ MultiLedger.Domain/Entities/Transaction.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using MultiLedger.Domain.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MultiLedger.Domain.Entities
+// Entities/Transaction.cs
+namespace MultiLedger.Domain.Entities;
+
+public class Transaction
 {
-    class Transaction
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public Guid BranchId { get; private set; }
+    public Money Amount { get; private set; }
+    public string Description { get; private set; }
+    public DateTime Date { get; private set; } = DateTime.UtcNow;
+
+    private Transaction() { }
+
+    public Transaction(Guid branchId, Money amount, string description)
     {
+        BranchId = branchId;
+        Amount = amount;
+        Description = description;
     }
 }
