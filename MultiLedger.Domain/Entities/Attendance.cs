08e12e35870f39cbbb301daa6a5043f7e5e1f5d2
Abubakar Mi <abubakarmi131@gmail.com>
2025-08-26 12:44:58 +0100

    attendance

--- MultiLedger.Domain/Entities/Attendance.cs
+++ MultiLedger.Domain/Entities/Attendance.cs
@@ -5,21 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 
 // Entities/Attendance.cs
-namespace MultiLedger.Domain.Entities;
-
-public class Attendance
+namespace MultiLedger.Domain.Entities
 {
-    public Guid Id { get; private set; } = Guid.NewGuid();
-    public Guid StaffId { get; private set; }
-    public DateTime Date { get; private set; }
-    public bool Present { get; private set; }
-
-    private Attendance() { }
-
-    public Attendance(Guid staffId, DateTime date, bool present)
+    public class Attendance
     {
-        StaffId = staffId;
-        Date = date;
-        Present = present;
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public Guid StaffId { get; private set; }
+        public DateTime Date { get; private set; }
+        public bool Present { get; private set; }
+
+        private Attendance() { }
+
+        public Attendance(Guid staffId, DateTime date, bool present)
+        {
+            StaffId = staffId;
+            Date = date;
+            Present = present;
+        }
     }
 }
