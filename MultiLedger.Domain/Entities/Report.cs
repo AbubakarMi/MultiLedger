298a6f011deeb17aca5cf22037415b02168c028b
Abubakar Mi <abubakarmi131@gmail.com>
2025-08-26 12:23:45 +0100

    report

--- MultiLedger.Domain/Entities/Report.cs
+++ MultiLedger.Domain/Entities/Report.cs
@@ -4,9 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MultiLedger.Domain.Entities
+// Entities/Report.cs
+namespace MultiLedger.Domain.Entities;
+
+public class Report
 {
-    class Report
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string Title { get; private set; }
+    public string Content { get; private set; }
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+
+    private Report() { }
+
+    public Report(string title, string content)
     {
+        Title = title;
+        Content = content;
     }
 }
