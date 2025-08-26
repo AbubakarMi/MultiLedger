ba419bd881ad20c521d502a0e196934ffbe85aed
Abubakar Mi <abubakarmi131@gmail.com>
2025-08-26 12:40:06 +0100

    money

--- MultiLedger.Domain/ValueObjects/Money.cs
+++ MultiLedger.Domain/ValueObjects/Money.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Bogus.DataSets;
+using PayPal.Api;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Currency = PayPal.Api.Currency;
 
-namespace MultiLedger.Domain.ValueObjects
+namespace MultiLedger.Domain.ValueObjects;
 {
-    class Money
+
+    public record Money
     {
+    public decimal Amount { get; }
+    public string Currency { get; }
+
+    public Money(decimal amount, string currency = "USD")
+    {
+        if (amount < 0) throw new ArgumentException("Amount cannot be negative");
+        amount = amount;
+    currency = currency;
     }
+
+    public override string ToString() => $"{Currency} {Amount:N2}";
 }
